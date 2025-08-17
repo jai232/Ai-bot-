bbccc4523ac7d049e93918e80027e6bd44e00376
Chandrakant <118928620+jai232@users.noreply.github.com>
2025-08-17 21:25:04 +0530

    Musiclibrary.py

---
+++ Musiclibrary.py
@@ -0,0 +1,10 @@
+music = {
+    "imagine": "https://www.youtube.com/watch?v=W0DM5lcj6mw",
+    "until": "https://www.youtube.com/watch?v=atRzwlp0iDk",
+    "night":"https://www.youtube.com/watch?v=r0mF6MoEM7M",
+    "saude":"https://www.youtube.com/watch?v=ONBuAW91LO8",
+    "passenger":"https://www.youtube.com/watch?v=6SjslBAJkJI",
+    "silence":"https://www.youtube.com/watch?v=lTFrfBCozHM",
+    "vaste":"https://www.youtube.com/watch?v=hG77NTZ0lhI",
+    "nadaniyan":"https://www.youtube.com/watch?v=gPpQNzQP6gE"
+}
